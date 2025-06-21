6b088660901dcebf6445d3d075febdb2aa7c2b46
KonstantineVasahalomize <vashalomidzekonstantine@gmail.com>
2025-06-21 19:49:37 +0400

    in one try

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> result = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            result.merge(value, key, Integer::min);
+        }
+        return result;
     }
+
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordFrequency = new HashMap<>();
+
+        for (String word : Arrays.stream(sentence.split("[^a-zA-Z]+"))
+                .filter(word -> !word.isEmpty()).toArray(String[]::new)) {
+            wordFrequency.merge(word.toLowerCase(), 1, Integer::sum);
+        }
+        return wordFrequency;
     }
 }
